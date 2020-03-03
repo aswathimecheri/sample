4727cfb6d8971706fd846108d9b737f150446c5d
aswathimecheri <48235310+aswathimecheri@users.noreply.github.com>
2020-03-02 21:59:04 -0800

    changed gray

--- gray.py
+++ gray.py
@@ -3,6 +3,8 @@ capture = cv2.VideoCapture(0)
 while(True):
     ret,frame = capture.read()
     grayFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    #hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    lab = cv2.cvtColor(frame,cv2.COLOR_BGR2LAB)
     hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     cv2.imshow('video gray',grayFrame)
     cv2.imshow('video original',frame)
